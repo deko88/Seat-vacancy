75779544a6ae752e721bd277d44fb7d9ed4219c2
Dejan Igumanovic <39744854+deko88@users.noreply.github.com>
2018-06-16 20:10:11 +0200

    Implemented Login

--- app/src/main/java/com/example/deko/classroom/SeatVacancy.java
+++ app/src/main/java/com/example/deko/classroom/SeatVacancy.java
@@ -2,17 +2,12 @@ package com.example.deko.classroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.Button;
-import android.widget.TextView;
 
 
 public class SeatVacancy extends AppCompatActivity implements View.OnClickListener{
-
-    String sittingPerson1, sittingPerson2, sittingPerson3, sittingPerson4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,53 +20,45 @@ public class SeatVacancy extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
 
-        EditText UserName = findViewById(R.id.userName);
-        String strUserName = UserName.getText().toString();
-
-
-        if(TextUtils.isEmpty(strUserName)) {
-            UserName.setError("Please enter your name");
-        } else {
+        Button btn1 = findViewById(R.id.seat1);
+        Button btn2 = findViewById(R.id.seat2);
+        Button btn3 = findViewById(R.id.seat3);
+        Button btn4 = findViewById(R.id.seat4);
 
 
             switch (v.getId()) {
-                case R.id.s1:
-                    Toast.makeText(this, "Seat 01 has been selected by " + strUserName, Toast.LENGTH_SHORT).show();
-                    Button btn1 = findViewById(R.id.s1);
+                case R.id.seat1:
+                    Toast.makeText(this, "Seat 01 has been selected by " + getIntent().getStringExtra("NAME"), Toast.LENGTH_SHORT).show();
                     btn1.setEnabled(false);
-                    sittingPerson1 = strUserName;
-                    TextView showSeat1 =findViewById(R.id.sP1);
-                    showSeat1.setText(sittingPerson1);
-                    break;
-                case R.id.s2:
-                    Toast.makeText(this, "Seat 02 has been selected by " + strUserName, Toast.LENGTH_SHORT).show();
-                    Button btn2 = findViewById(R.id.s2);
                     btn2.setEnabled(false);
-                    sittingPerson2 = strUserName;
-                    TextView showSeat2 =findViewById(R.id.sP2);
-                    showSeat2.setText(sittingPerson2);
-                    break;
-                case R.id.s3:
-                    Toast.makeText(this, "Seat 03 has been selected by " + strUserName, Toast.LENGTH_SHORT).show();
-                    Button btn3 = findViewById(R.id.s3);
                     btn3.setEnabled(false);
-                    sittingPerson3 = strUserName;
-                    TextView showSeat3 =findViewById(R.id.sP3);
-                    showSeat3.setText(sittingPerson3);
-                    break;
-                case R.id.s4:
-                    Toast.makeText(this, "Seat 04 has been selected by " + strUserName, Toast.LENGTH_SHORT).show();
-                    Button btn4 = findViewById(R.id.s4);
                     btn4.setEnabled(false);
-                    sittingPerson4 = strUserName;
-                    TextView showSeat4 =findViewById(R.id.sP4);
-                    showSeat4.setText(sittingPerson4);
+                    break;
+                case R.id.seat2:
+                    Toast.makeText(this, "Seat 02 has been selected by " + getIntent().getStringExtra("NAME"), Toast.LENGTH_SHORT).show();
+                    btn1.setEnabled(false);
+                    btn2.setEnabled(false);
+                    btn3.setEnabled(false);
+                    btn4.setEnabled(false);
+                    break;
+                case R.id.seat3:
+                    Toast.makeText(this, "Seat 03 has been selected by " + getIntent().getStringExtra("NAME"), Toast.LENGTH_SHORT).show();
+                    btn1.setEnabled(false);
+                    btn2.setEnabled(false);
+                    btn3.setEnabled(false);
+                    btn4.setEnabled(false);
+                    break;
+                case R.id.seat4:
+                    Toast.makeText(this, "Seat 04 has been selected by " + getIntent().getStringExtra("NAME"), Toast.LENGTH_SHORT).show();
+                    btn1.setEnabled(false);
+                    btn2.setEnabled(false);
+                    btn3.setEnabled(false);
+                    btn4.setEnabled(false);
                     break;
 
             }
 
         }
 
-}
 }
 
---
+++ app/src/main/java/com/example/deko/classroom/SignInPage.java
@@ -0,0 +1,65 @@
+package com.example.deko.classroom;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
+
+
+public class SignInPage extends AppCompatActivity {
+
+    EditText Name;
+    EditText Password;
+    Button Login;
+
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_page);
+
+        Name = findViewById(R.id.userName);
+        Password = findViewById(R.id.passWord);
+        Login = findViewById(R.id.signIn);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    public void validate(String userName, String userPassword) {
+
+
+
+            if (TextUtils.isEmpty(userName)) {
+                Name.setError("Please enter your name");
+
+            } else if (TextUtils.isEmpty(userPassword)) {
+                Password.setError("Please enter your password");
+
+            } else if (((userName.equals("Mike")) && (userPassword.equals("Tuna"))) || ((userName.equals("Joe")) && (userPassword.equals("Swordfish"))) || ((userName.equals("Becca")) && (userPassword.equals("HammerShark")))) {
+                Intent intent = new Intent(getApplicationContext(), SeatVacancy.class);
+                intent.putExtra("NAME", userName);
+                startActivity(intent);
+
+            } else {
+                    {
+                        counter--;
+
+                        Toast.makeText(this, "Please enter the correct Username or Password. You have " + counter + " attempts remaining.", Toast.LENGTH_LONG).show();
+
+                        if (counter == 0) {
+                            Login.setEnabled(false);
+                        }
+                    }
+            }
+    }
+}
