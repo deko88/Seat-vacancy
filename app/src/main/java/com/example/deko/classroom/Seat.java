0f11c23100b686cfebb3bc0398c7491208bd1885
Dejan Igumanovic <39744854+deko88@users.noreply.github.com>
2018-07-06 23:30:15 +0200

    Added Seat Class

---
+++ app/src/main/java/com/example/deko/classroom/Seat.java
@@ -0,0 +1,21 @@
+package com.example.deko.classroom;
+
+public class Seat {
+
+    private String number;
+
+    public Seat() {
+    }
+
+    public Seat(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
